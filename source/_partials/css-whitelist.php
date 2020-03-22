fe1cb82c5845d4d5ce02b9dc81bfc4383764d421
Daragh O'Shea <daraghoshea@gmail.com>
2020-03-22 03:54:16 +0000

    add css whitelist to prevent dynamic classnames from being purged in production

---
+++ source/_partials/css-whitelist.php
@@ -0,0 +1,8 @@
+<div class="
+    xl:block lg:block md:block sm:block
+    xl:hidden lg:hidden md:hidden sm:hidden
+    xl:ml-auto lg:ml-auto md:ml-auto sm:ml-auto
+    xl:pr-4 lg:pr-4 md:pr-4 sm:pr-4
+    xl:hidden lg:hidden md:hidden sm:hidden
+    xl:w-auto lg:w-auto md:w-auto sm:w-auto
+"></div>
